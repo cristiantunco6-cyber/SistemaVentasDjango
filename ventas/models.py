0f1713c34e6a360111325eaa21f6390417f595c4
cristiantunco6-cyber <cristiantunco6@gmail.com>
2026-09-05 22:37:37 -0500

    Agrego modelos Categoria Producto y Proveedor

--- ventas/models.py
+++ ventas/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    precio_actual = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='productos')
+
+    def __str__(self):
+        return self.nombre
+
+
+class Proveedor(models.Model):
+    codigo = models.CharField(max_length=20, unique=True)
+    nombre = models.CharField(max_length=100)
+    calle = models.CharField(max_length=100)
+    numero = models.CharField(max_length=10)
+    comuna = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20)
+    pagina_web = models.URLField(blank=True)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, related_name='proveedores')
+
+    def __str__(self):
+        return self.nombre
